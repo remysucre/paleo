b12c2822e04f2af4bf20313cb5af237548d04015
Remy Wang <remywang@uw.edu>
2018-05-02 15:06:54 -0700

    cleanup

--- paleo.rkt
+++ paleo.rkt
@@ -1,3 +1,5 @@
+#lang racket
+
 ; procedure Synthesize(G, Ψ, Φ)
 ;   P ← Root(S)
 ;   Ω ← ∅
@@ -19,11 +21,48 @@
 ; the chosen programming language. Formulate data definitions and illustrate
 ; them with examples.
 
-; G grammar, Ψ semantics, Φ spec
-; Ω knowledge base Ω (set of learned lemmas)
-; P partial program, S start symbol
-; (H,p) hole and candidate production
-; κ conflict
+;https://docs.racket-lang.org/htdp-langs/advanced.html?q=grammar#%28form._%28%28lib._lang%2Fhtdp-advanced..rkt%29._define-datatype%29%29
+
+; G: Standard grammar rules
+(struct Grammar (V sigma R S))
+    ; V: Non-terminals
+(define V '(N T P A C CL F)) ; Set of rule names (e.g. {N T P A C CL F} in paper)
+    ; Sigma: terminals
+(define sigma '(x -3 -2 -1 0 1 2 3 last head access ...)) ; Set of terminals (e.g. {x -3 -2 -1 0 1 2 3 last head access ...})
+    ; R: production rules
+(define R '((L, sort(L) reverse(L) filter(L T N) ...) ...)) ; List of tuples of names and arguments (e.g. {(L, sort(L) reverse(L) filter(L,T,N) ...) ...})
+    ; S: start symbol
+(define S 'N) ; Specific rule: non-terminal name
+
+; Psi: semantics - mapping from terminals to first-order formulae
+(define Psi #hash((inc . (lambda (x y) (= y (+ x 1)))))) ; terminals -> formulae
+
+; Phi: specification - user defined smt formulae
+(define Phi (lambda (Lin Lout) (and (>= (length Lin) 1) (= (length Lout) 1)))) ; Look at bottom of paper
+
+; P: partial program - AST with holes, built from S via production rules
+(define P1 '(0 N head
+                (1 L take
+                    ((3 L filter ((7 L x1)
+                                  (8 T)))
+                     (4 N)))))
+
+; Omega: knowledge base - set of SAT formulae
+(define Omega '((disj (neg c 0 filter) (neg c 2 eqz))
+                (disj (neg c 0 filter) (neg c 2 leqz))))
+
+; H: hole - unfilled piece of the partial program
+(define hole 4)
+
+; p: candidate production - to fill in the hole
+(define p1 'filter)
+
+; kappa: conflict
+; (y ≤ x1.max, N0, head), (y.max ≤ x1.max, N1, take), (y.max ≤ x1.max,N3,filter), (y = x1,N7,x1)
+(define kappa '(((leq y (max x1)) 0 'head)
+                ((leq ((max y) (max x1))) 1 'take)
+                ((leq (max y) (max x1)) 3 'filter)
+                ((= y x1) 7 'x1)))
 
 ; Signature, Purpose Statement, Header
 ;
@@ -31,32 +70,7 @@
 ; a concise answer to the question what the function computes. Define a stub
 ; that lives up to the signature.
 
-; G: Standard grammar rules
-    ; V: Non-terminals
-    ; Sigma: terminals
-    ; R: production rules
-    ; S: start symbol
-; Psi: semantics - mapping from terminals to first-order formulae
-; Phi: specification - user defined smt formulae
-; Omega: knowledge base - set of SAT formulae
-; P: partial program - AST with holes, built from S via production rules
-; H: hole - unfilled piece of the partial program
-; p: candidate production - to fill in the hole
-; kappa: conflict
-
-;https://docs.racket-lang.org/htdp-langs/advanced.html?q=grammar#%28form._%28%28lib._lang%2Fhtdp-advanced..rkt%29._define-datatype%29%29
-
-(define V '(N T P A C CL F)) ; Set of rule names (e.g. {N T P A C CL F} in paper)
-(define sigma '(x -3 -2 -1 0 1 2 3 last head access ...)) ; Set of terminals (e.g. {x -3 -2 -1 0 1 2 3 last head access ...})
-(define R '((L, sort(L) reverse(L) filter(L T N) ...) ...)) ; List of tuples of names and arguments (e.g. {(L, sort(L) reverse(L) filter(L,T,N) ...) ...})
-(define S 'N) ; Specific rule: non-terminal name
-(struct Grammar (V sigma R S))
-(define Psi #hash((inc . (lambda (x y) (= y (+ x 1)))))) ; terminals -> formulae
-(define Phi (lambda (Lin Lout) (and (>= (len Lin) 1) (= (len Lout) 1)))) ; Look at bottom of paper
-
-(define Omega '((or (not c0...) (not c1...) ...) ...))
-
-(define (paleo G Psi Phi))
+(define (synth G Psi Phi) '())
 
 ; Functional Examples
 ;
