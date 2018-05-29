fac03bb424d311f960ff24288ca5bc101a13717a
Remy Wang <remywang@uw.edu>
2018-05-29 11:20:31 -0700

    8

--- conflict.rkt
+++ conflict.rkt
@@ -4,6 +4,8 @@
 
 (provide (all-defined-out))
 
+; CHECKCONFLICT
+
 ; procedure CheckConflict(P, Ψ, Φ)
 ;  ΦP ← InferSpec(P)
 ;  ψ  ← SMTSolve(ΦP ∧ Φ)
@@ -11,24 +13,101 @@
 ;  κ' ← {(φ',N, χN) | φ'= Rename(φ) ∧ (φ, N, χN) ∈ κ}
 ;  return k'
 
-(define (declare-v id) (list 'declare-const (string->symbol (string-append "v" (number->string id))) '(List Int)))
+; returns the MUC of a conflict
+(define (CheckConflict P Psi Phi)
+  (print 'checkcstart)
+  (define (phi-n-xn muc)
+    (let* ([id (string->number (substring (symbol->string muc) 1))]
+           [xn (Partial-Terminal (Lookup-By-ID P id))]
+           [ph (sem xn Psi)])
+      (list ph id xn)))
+  (define (declare-xs phi)
+    (cons '(declare-const y (List Int))
+          (map (lambda (x) (list 'declare-const x '(List Int)))
+               (filter (lambda (c) (string-prefix? (symbol->string c) "x")) (set->list (list->set (filter symbol? (flatten phi))))))))
+  (let* ([Phi-P (InferSpec P Psi)]
+         [encoding (append (declare-xs Phi) Phi-P (list (list 'assert (list '! Phi ':named 'aphi))))]
+         [psi0 (SMTSolve encoding)]
+         [psi (if (list? psi0) psi0 '())]
+         [k (map phi-n-xn (filter (lambda (x) (not (equal? x 'aphi)))psi))])
+    (print k) k))
 
-(define (declare-vs p)
-  (for/list ([n p]) (declare-v (Partial-ID n))))
+(define (InferSpec p Psi)
+  (define (declare-vs p)
+    (for/list ([n p])
+        (list 'declare-const (v_s (Partial-ID n)) '(List Int))))
+  (define (phi-p p Psi)
+    (subst (v_s (Partial-ID p)) 'y (phi-n p Psi)))
+  (define (phi-n p Psi)
+    (for/list ([n p])
+      (list 'assert (list '! (psi-n n Psi) ':named
+                          (string->symbol (string-append "a" (number->string (Partial-ID n))))))))
+  (define (psi-n p Psi)
+    (if (not (Partial-Filled? p))
+        'true
+        (if (null? (Partial-Children p))
+            (if (number? (Partial-Terminal p))
+                (list '= (v_s (Partial-ID p)) (list 'insert (Partial-Terminal p) 'nil))
+                (list '= (v_s (Partial-ID p)) (Partial-Terminal p)))
+            (substs (xs-of (sem (Partial-Terminal p) Psi))
+                    (C (Partial-Children p))
+                    (subst 'y (v_s (Partial-ID p)) (sem (Partial-Terminal p) Psi))))))
+  (define (xs-of s) (set-intersect (flatten s) '(x1 x2))) ; do this properly
+  (define (C cs) (map (lambda (n) (v_s (Partial-ID n))) cs))
+  (define (v_s id) (string->symbol (string-append "v" (number->string id))))
+  (let ([vs (declare-vs p)]
+        [phi-ns (phi-p p Psi)]) (append vs phi-ns)))
 
-  ; [((list id A p)) (list (declare-v id))]
-  ; [((list id A p cs)) (flatten (cons (declare-v id) (map declare-vs cs)))])
-
-;(define/match (declare-vs p Psi)
-;  [(list id A p cs) (cons Psi-of p Psi)]
-;  [(list id A 'HOLE)])
-
-(define (v_s id) (string->symbol (string-append "v" (number->string id))))
 (define (sem p Psi)
-  (if (dict-has-key? Psi p)
-      (dict-ref Psi p)
-          (list '= 'y p)))
+  (print p)
+  (print (number? p))
+  (dict-ref Psi p
+            (if (number? p)
+                (list '= 'y (list 'insert p 'nil))
+                (list '= 'y p))))
 
+; ANALYZECONFLICT
+
+
+; Partial-Tree: 
+
+; 1: N, 0, #t
+; '(#hash((N . (0 1 2 3 4 5 6 7 8 9 10 x1 x2 x3 x4 x5 (last L) (head L) (sum L) (maximum L) (minimum L))) (T . (geqz leqz eqz)) (L . ((take L N) (filter L T) (sort L) (reverse L))))
+; #hash((minimum . (and (> (len x1) 1) (= (len y) 1) (>= (max x1) (max y)) (= (min x1) (min y)))) (sum . (and (>= (len x1) 1) (= (len y) 1))) (reverse . (and (= (len y) (len x1)) (> (len x1) 1) (= (max x1) (max y)) (= (min x1) (min y)) (= (first x1) (last y)) (= (last x1) (first y)))) (maximum . (and (> (len x1) 1) (= (len y) 1) (= (max x1) (max y)) (<= (min x1) (min y)))) (take . (and (< (len y) (len x1)) (>= (max x1) (max y)) (<= (min x1) (min y)) (> (head x2) 0) (> (len x1) (head x2)) (= (first x1) (first y)))) (last . (and (>= (len x1) 1) (= (len y) 1) (>= (max x1) (max y)) (<= (min x1) (min y)) (= (first y) (last x1)) (= (last y) (last x1)))) (filter . (and (< (len y) (len x1)) (= x2 x2) (>= (max x1) (max y)) (<= (min x1) (min y)))) (0 . (= y 0)) (sort . (and (= (len y) (len x1)) (> (len x1) 1) (= (max x1) (max y)) (= (min x1) (min y)))) (head . (and (>= (len x1) 1) (= (len y) 1) (>= (max x1) (max y)) (<= (min x1) (min y)) (= (first y) (first x1)) (= (last y) (first x1)))))
+; (((= y 0) 1 0)))
+
+; learn lemmas from the MUC of a conflict
+(define (AnalyzeConflict P gamma Psi kappa)
+  (print 'begin)
+  (print-partial P)
+  (print (list gamma Psi kappa))
+  (print 'end)
+    (for/fold ([sphi '()]) ([clause kappa])
+      (match-define (list phi node p0) clause)
+      (define As (Partial-Children (Lookup-By-ID P node)))
+      (define rules gamma)
+      #;(define sigma (for*/list ([A (cons (Lookup-By-ID P node) As)]
+                                [prod (dict-ref rules (Partial-Non-Terminal A))]
+                                #:when (list? prod)
+                                #:when (list? (SMTSolve `((declare-const x1 (List Int))
+                                                          (declare-const x2 (List Int))
+                                                          (declare-const y (List Int))
+                                                          (assert
+                                                           (and ,(sem (Production-Terminal prod) Psi)
+                                                                (not ,phi)))))))
+                      (list (Partial-ID A) (Production-Terminal prod))))
+      (append sphi (list (list (cons node p0))))))
+
+; EXAMPLES
+
+; grammar
+(define R1
+  (make-immutable-hash
+   (list (cons 'N (append `,(range 0 11) '(x1 x2 x3 x4 x5) '((last L) (head L) (sum L) (maximum L) (minimum L))))
+         (cons 'L (list '(take L N) '(filter L T) '(sort L) '(reverse L)))
+         (cons 'T '(geqz leqz eqz)))))
+
+; semantics
 (define Psi0 #hash((filter . (and (< (len y) (len x1)) (= x2 x2) (>= (max x1) (max y)) (<= (min x1) (min y))))
                   (minimum . (and (> (len x1) 1) (= (len y) 1) (>= (max x1) (max y)) (= (min x1) (min y))))
                   (maximum . (and (> (len x1) 1) (= (len y) 1) (= (max x1) (max y)) (<= (min x1) (min y))))
@@ -37,75 +116,7 @@
                   (sum . (and (>= (len x1) 1) (= (len y) 1)))
                   (take . (and (< (len y) (len x1)) (>= (max x1) (max y)) (<= (min x1) (min y)) (> (head x2) 0) (> (len x1) (head x2)) (= (first x1) (first y))))
                   (reverse . (and (= (len y) (len x1)) (> (len x1) 1) (= (max x1) (max y)) (= (min x1) (min y)) (= (first x1) (last y)) (= (last x1) (first y))))
-                  (sort . (and (= (len y) (len x1)) (> (len x1) 1) (= (max x1) (max y)) (= (min x1) (min y))))
-                  (0 . (= y 0))))
-
-;(sem 'last Psi0)
-
-(define (C cs) (map (lambda (n) (v_s (Partial-ID n))) cs))
-
-(define (xs-of s) (set-intersect (flatten s) '(x1 x2))) ; do this properly
-
-;(xs-of '(Lout \. len = Lin \. len > 1 ∧ Lin \. max = Lout \. max ∧ Lin \. min = Lout \. min ∧ Lin \. first = Lout \. last ∧ Lin \. last = Lout \. first))
-
-(define (phi-p p Psi) (subst (v_s (Partial-ID p)) 'y (phi-n p Psi)))
-
-(define (psi-n p Psi)
-  (if (not (Partial-Filled? p))
-      'true
-      (if (null? (Partial-Children p))
-          (if (number? (Partial-Terminal p))
-              (list '= (v_s (Partial-ID p)) (list 'insert (Partial-Terminal p) 'nil))
-              (list '= (v_s (Partial-ID p)) (Partial-Terminal p)))
-          
-          (substs (xs-of (sem (Partial-Terminal p) Psi)) (C (Partial-Children p)) (subst 'y (v_s (Partial-ID p)) (sem (Partial-Terminal p) Psi))))))
-
-(define (phi-n p Psi)
-  (for/list ([n p]) (list 'assert (list '! (psi-n n Psi) ':named (string->symbol (string-append "a" (number->string (Partial-ID n)))))))) ; (assert (! false :named a6) )
-
-;(Partial-Children P1)
-
-(define (InferSpec p Psi)
-  (let ([vs (declare-vs p)]
-        [phi-ns (phi-p p Psi)]) (append vs phi-ns)))
-
-;(InferSpec P1 Psi0)
-
-(define (declare-xs phi)
-  (cons '(declare-const y (List Int))
-        (map (lambda (x) (list 'declare-const x '(List Int)))
-               (filter (lambda (c) (string-prefix? (symbol->string c) "x")) (set->list (list->set (filter symbol? (flatten phi))))))))
-
-; returns the MUC of a conflict
-(define (CheckConflict P Psi Phi)
-  ;(print 'checkc)
-  (define (Chi n) (Partial-Terminal (Lookup-By-ID P n)))
-  (define (phi-n-xn muc)
-    (let* ([id (string->number (substring (symbol->string muc) 1))]
-           [xn (Chi id)]
-           [ph (sem xn Psi)])
-      (list ph id xn)))
-  (let* ([Phi-P (InferSpec P Psi)]
-         [encoding (append (declare-xs Phi) Phi-P (list (list 'assert (list '! Phi ':named 'aphi))))]
-         [psi0 (SMTSolve encoding)]
-         [psi (if (list? psi0) psi0 '())]
-         [k (map phi-n-xn (filter (lambda (x) (not (equal? x 'aphi)))psi))]
-         #;[k_ (map (match-lambda [(list phi N X) (list (Rename phi) (Node phi) (Chi (Node phi)))]) k)])
-    (print k)k))
-
-; learn lemmas from the MUC of a conflict
-#;(define (AnalyzeConflict P gamma Psi kappa)
-  (define (step phi x)
-    (match x [(list ph N X)
-              (match-let* ([As (map (lambda (Ni) (Aof Ni)) (Children N))]
-                           [sig (filter (lambda (x) (implies (Of Psi x) ph)) (sig-of As))])
-                (disj ph (foldr conj #t (map (lambda (x) (neg (assigned-to N x))) sig))))]))
-  (foldr (lambda (ki phi) step phi ki) #f kappa))
-
-(define R1 (make-immutable-hash
-           (list (cons 'N (append `,(range 0 11) '(x1 x2 x3 x4 x5) '((last L) (head L) (sum L) (maximum L) (minimum L))))
-             (cons 'L (list '(take L N) '(filter L T) '(sort L) '(reverse L)))
-             (cons 'T '(geqz leqz eqz)))))
+                  (sort . (and (= (len y) (len x1)) (> (len x1) 1) (= (max x1) (max y)) (= (min x1) (min y))))))
 
 (define Psi #hash((filter . (and (< (len y) (len x1)) (>= (max x1) (max y)) (<= (min x1) (min y))))
                   (minimum . (and (> (len x1) 1) (= (len y) 1) (>= (max x1) (max y)) (= (min x1) (min y))))
@@ -116,95 +127,5 @@
                   (take . (and (< (len y) (len x1)) (>= (max x1) (max y)) (<= (min x1) (min y)) (> (head x2) 0) (> (len x1) (head x2)) (= (first x1) (first y))))
                   (reverse . (and (= (len y) (len x1)) (> (len x1) 1) (= (max x1) (max y)) (= (min x1) (min y)) (= (first x1) (last y)) (= (last x1) (first y))))
                   (sort . (and (= (len y) (len x1)) (> (len x1) 1) (= (max x1) (max y)) (= (min x1) (min y))))))
-  
-(define (AnalyzeConflict P gamma Psi kappa)
-  ;(print 'analyzec)
-    (for/fold ([sphi '()]) ([clause kappa])
-      (match-define (list phi node prod) clause)
-      (define As (Partial-Children (Lookup-By-ID P node)))
-      ;(define rules (caddr gamma))
-      (define rules gamma)
-      (define sigma (for*/list ([node As]
-                                [prod (dict-ref rules (Partial-Non-Terminal node))]
-                                #:when (list? prod)
-                                #:when (not (SMTSolve `((declare-const x1 (List Int))
-                                                        (declare-const x2 (List Int))
-                                                        (declare-const y (List Int))
-                                                        (assert
-                                                         (and ,(dict-ref Psi (Production-Terminal prod))
-                                                         (not ,phi)))))))
-                      (list (Partial-ID node) (Production-Terminal prod))))
-      (print (append sphi sigma))
-      (append sphi sigma)))
 
-;Test
-;(AnalyzeConflict P1 R1 Psi '(((= (len y) 1) 1 maximum)))
-
-(define (Aof x) '()) ; 
-(define (Children x) '()) ; TODO
-(define (Of x) '()) ; TODO
-(define (sig-of x) '()) ; TODO
-(define (disj x) '()) ; TODO
-(define (neg x) '()) ; TODO
-(define (assigned-to x) '()) ; TODO
-
-(define P2 '(0 N head
-                ((1 L take
-                    ((3 L filter ((7 L x1)
-                                  (8 T HOLE)))
-                     (4 N HOLE))))))
-
-; (declare-vs P2)
-
-; (Backtrack ((disj (neg c 0 filter) (neg c 2 eqz)) (disj (neg c 0 filter) (neg c 2 leqz)))) (0 1) (2 2) (0 p0) (1 P2) (2 p2)
-; lookup 2 pps
-
-(define (d2l d) (dict-map d (lambda (x y) (cons x y))))
-
-;(define (revert l xs) (make-immutable-hash (filter (lambda (x) (< (car x) l)) (d2l xs))))
-
-(define (revert l xs) (filter (lambda (x) (<= (car x) l)) (d2l xs)))
-
-;(revert 3 (list (cons 1 2) (cons 2 4) (cons 4 5)))
-
-; takes in a conflict, the decision history and the partial progam history
-; returns the partial program at the second highest decision level in the conflict
-(define (backtrack omega ds pps)
-  (print pps)
-  (let* ([snd-l (if (< (length omega) 2) 0 (cadadr (sort-with omega ds)))]
-        [pp (dict-ref pps snd-l)]
-        [pps_ (revert snd-l pps)]
-        [ds_ (if (= 0 snd-l) '() (revert snd-l ds))])
-    (list pp pps_ ds_)))
-
-; backtrack finds the second highest decision level in the MUC omega according to ds
-; then returns the partial program at that level from pps
-; as well as reverting ds and pps to that level
-
-; a partial program
-
-; pairs of decision level and partial program
-
-(define pps-eg (list (cons 1 P2)))
-
-; pairs of decision level and node id
-(define ds-eg #hash((1 . 2) (0 . 3) (7 . 1) (3 . 5)))
-
-; sort the conflict with decision level
-
-(define (sort-with omega ds)
-  (print ds)
-  (define (less x y) (< (dict-ref ds (cadr x)) (dict-ref ds (cadr y))))
-  (sort omega less))
-
-; unsat core with decision
-(define om-eg '(((leq y (max x1)) 0 'head)
-                ((leq ((max y) (max x1))) 1 'take)
-                ((leq (max y) (max x1)) 3 'filter)
-                ((= y x1) 7 'x1)))
-
-;(backtrack om-eg ds-eg pps-eg)
-
-;(declare-vs P1)
-
-;(CheckConflict P1 Psi0 '(> (len x1) (len y)))
+(sem 0 Psi)
--- paleo.rkt
+++ paleo.rkt
@@ -1,6 +1,23 @@
 #lang racket
 (require rackunit "conflict.rkt" "decide.rkt" "partial.rkt")
 
+; BACKTRACK
+; 
+; backtrack finds the second highest decision level in the MUC omega according to ds
+; then returns the partial program at that level from pps
+; as well as reverting ds and pps to that level
+
+; takes in a conflict, the decision history and the partial progam history
+; returns the partial program at the second highest decision level in the conflict
+(define (backtrack omega ds pps)
+  (define (revert l xs) (filter (lambda (x) (<= (car x) l)) xs))
+  (define (less x y) (< (dict-ref ds (cadr x)) (dict-ref ds (cadr y))))
+  (let* ([snd-l (if (< (length omega) 2) 0 (cadadr (sort omega less)))]
+         [pp (dict-ref pps snd-l)]
+         [pps_ (revert snd-l pps)]
+         [ds_ (if (= 0 snd-l) '() (revert snd-l ds))])
+    (list pp pps_ ds_)))
+
 ; procedure Synthesize(G, Ψ, Φ)
 ;   P ← Root(S)
 ;   Ω ← ∅
@@ -15,113 +32,18 @@
 ;        return ⊥
 ;     else if IsConcrete(P) then
 ;        return P
-
-; From Problem Analysis to Data Definitions
-;
-; Identify the information that must be represented and how it is represented in
-; the chosen programming language. Formulate data definitions and illustrate
-; them with examples.
-
-;https://docs.racket-lang.org/htdp-langs/advanced.html?q=grammar#%28form._%28%28lib._lang%2Fhtdp-advanced..rkt%29._define-datatype%29%29
-
-; G: Standard grammar rules
-(struct Grammar (V sigma R S))
-    ; V: Non-terminals
-(define V '(N L T)) ; Set of rule names (e.g. {N T P A C CL F} in paper)
-    ; Sigma: terminals
-(define sigma (append `,(range 0 11)
-                      '(x1 x2 x3 x4 x5 last head sum maximum minimum)
-                      '(take filter sort reverse)
-                      '(geqz leqz eqz))) ; Set of terminals (e.g. {x -3 -2 -1 0 1 2 3 last head access ...})
-    ; R: production rules
-(define R (make-immutable-hash
-           (list (cons 'N (append `,(range 0 11) '(x1 x2 x3 x4 x5) '((last L) (head L) (sum L) (maximum L) (minimum L))))
-             (cons 'L (list '(take L N) '(filter L T) '(sort L) '(reverse L)))
-             (cons 'T '(geqz leqz eqz))))) ; List of tuples of names and arguments (e.g. {(L, sort(L) reverse(L) filter(L,T,N) ...) ...})
-
-; S: start symbol
-(define S 'N) ; Specific rule: non-terminal name
-
-; last(L) | head(L) | sum(L) | maximum(L) | minimum(L) | take(L,N) | filter(L,T)| sort(L) | reverse(L) |  geqz | leqz | eqz
-; Psi: semantics - mapping from terminals to first-order formulae
-
- ; terminals -> formulae
-#;(define Psi #hash((last    . "Lin.len ≥ 1 ∧ Lout .len = 1 ∧ Lin.max ≥ Lout.max ∧ Lin.min ≤ Lout.min ∧ Lout.first = Lin.last ∧ Lout.last = Lin.last")
-                  (head    . "Lin.len ≥ 1 ∧ Lout .len = 1 ∧ Lin.max ≥ Lout.max ∧ Lin.min ≤ Lout.min ∧ Lout.first = Lin.first ∧ Lout.last = Lin.first")
-                  (sum     . "Lin.len ≥ 1 ∧ Lout .len = 1")
-                  (maximum . "Lin.len > 1 ∧ Lout.len = 1 ∧ Lin.max = Lout.max ∧ Lout.min ≥ Lin.min")
-                  (minimum . "Lin.len > 1 ∧ Lout.len = 1 ∧ Lin.max ≥ Lout.max ∧ Lout.min = Lin.min")
-                  (take    . "Lout.len < Lin.len Lin.max ≥ Lout.max Lin.min ≤ Lout.min k > 0 ∧ Lin.len > k Lin.first = Lout.first")
-                  (filter  . "Lout.len < Lout.len Lout.max ≤ Lin.max Lout.min ≥ Lin.min")
-                  (sort    . "Lout.len = Lin.len > 1 ∧ Lin.max = Lout.max ∧ Lin.min = Lout .min")
-                  (reverse . "Lout.len = Lin.len > 1 ∧ Lin.max = Lout.max ∧ Lin.min = Lout.min ∧ Lin.first = Lout.last ∧ Lin.last = Lout.first")))
-
-; Phi: specification - user defined smt formulae
-(define Phi '(and (>= (len x1) 1)
-                  (= 1 (len y))
-                  (>= (max x1) (max y))
-                  (<= (min x1) (min y))
-                  (> (head x2) 0 )
-                  (= (len x2) 1)
-                  (> (len x1) (head x2)))) ; Look at bottom of paper
-
-(define Phieg '(and (= 82 (max x1)) (= 158 (head y))))
-
-; P: partial program - AST with holes, built from S via production rules
-(define P1 '(0 N head
-                (1 L take
-                    ((3 L filter ((7 L x1)
-                                  (8 T HOLE)))
-                     (4 N HOLE)))))
-
-; Omega: knowledge base - set of SAT formulae
-(define Omega '((disj (neg c 0 filter) (neg c 2 eqz))
-                (disj (neg c 0 filter) (neg c 2 leqz))))
-
-; H: hole - unfilled piece of the partial program
-(define hole 4)
-
-; p: candidate production - to fill in the hole
-(define p1 'filter)
-
-; kappa: conflict
-; (y ≤ x1.max, N0, head), (y.max ≤ x1.max, N1, take), (y.max ≤ x1.max,N3,filter), (y = x1,N7,x1)
-(define kappa '(((leq y (max x1)) 0 'head)
-                ((leq ((max y) (max x1))) 1 'take)
-                ((leq (max y) (max x1)) 3 'filter)
-                ((= y x1) 7 'x1)))
-
-; Signature, Purpose Statement, Header
-;
-; State what kind of data the desired function consumes and produces. Formulate
-; a concise answer to the question what the function computes. Define a stub
-; that lives up to the signature.
-
-(define (Root S)
-  (list 0 S 'HOLE))
-
-(check-equal? (Root S) '(0 N HOLE))
-
-(define (IsConcrete P) (null? (Holes P)))
-
 (define (synth gamma Psi Phi)
-  ;; initialize variables
-  ;;
   (define P0 (Partial 1 'N 'HOLE #f '())) ; inital partial program
   (define omega0 '())  ; initial knowledge base
   (define ds0 '())     ; initial decision history
   (define pps0 (list (cons 0 P0))) ; initial partial program history
-  (define l0 0)        ; NOTE might be off by 1
-
+  (define l0 0)        ; initial decision level
   ;; wtd takes partial prog, knowledge base, decision history, 
   ;; partial program history
   ;;
   (define (wtd P Omega l ds pps)
     (print-partial P)
-    (print Omega)
-    (print l)
-    (print ds)
-    (print pps)
+    (print (list Omega l ds pps))
     ; decide to fill hole H with production pr
     (match-define (cons H pr) (Decide P gamma Phi Omega gamma)) ; TODO one gama
     ; propagate assignment and update partial program
@@ -130,9 +52,7 @@
     (define ds1 (cons (cons l H) ds)) ; TODO should update with propagate restul NOTE H should be int
     (define pps1 (cons (cons l P1) pps))
     ; check for conflict
-    (print-partial P1)
     (define kappa (CheckConflict P1 Psi Phi))
-    (print kappa)
     ; backtrack if there is conflict
     (match-let* (; analyze conflict and return MUC
                  [OmegaK (AnalyzeConflict P1 gamma Psi kappa)]
@@ -144,10 +64,12 @@
                  [(list P2 pps2 ds2) (if (not (null? kappa))
                                          (backtrack OmegaK ds pps)
                                          (list P1 ds1 pps1))])
+      (print 'OmegaK)
+      (print OmegaK)
       (cond
         [(Unsat P2 Omega1 gamma) #f]
-        [(IsConcrete P2) P2]
+        [(null? (Holes P2)) P2]
         [else (wtd P2 Omega1 (+ 1 l) ds2 pps2)])))
   (print-partial (wtd P0 omega0 l0 ds0 pps0)))
-(synth R1 Psi0 Phieg)
 
+(synth R1 Psi0 '(and (= x1 x1) (= 8 (head y))))
